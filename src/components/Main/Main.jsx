2b9d49b914d04f3e98b26822bfabd495a16079a5
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-05-25 00:22:56 +0100

    refactoring

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Col, Row } from 'antd';
+import { Link } from 'react-router-dom';
+import '../../styles/main.css'
+import football from '../../assets/football.jpg'
+import basketball from '../../assets/basketball.jpg'
+import cycling from '../../assets/cycling.jpg'
+import rugby from '../../assets/rugby.jpg'
+
+
 
 const Main = () => {
     return (
-        <main>
+        <main className='main'>
+            <Row className='sport-images-container'>
+                <Col span={12}>
+                    <img src={cycling} alt="cycling" />
+                    <div>
+                        {/* <Link className="" to="/cycling">Cycling</Link>
+                        <p>Lorem, ipsum dolor.</p> */}
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div className="football-container">
+                        <img src={football} alt="football" className='football' />
+                        {/* <Link to="/football">Football</Link>
+                        <p>Lorem, ipsum dolor.</p> */}
+                    </div>
+
+                    <div className='sport-showcase'>
+                        <div className="basketball-container">
+                            <img src={basketball} alt="basketball" />
+                            {/* <Link to="/basketball">Basketball</Link>
+                            <p>Lorem, ipsum dolor.</p> */}
+                        </div>
+                        <div className="rugby-container">
+                            <img src={rugby} alt="rugby" />
+                            {/* <Link to="/rugby">Rugby</Link>
+                            <p>Lorem, ipsum dolor.</p> */}
+                        </div>
+
+
+                    </div>
+                </Col>
+            </Row>
 
         </main>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Input } from 'antd';
+import '../../styles/navbar.css'
+import hamburger from '../../assets/icon-hamburger.svg'
 
 const Navbar = () => {
+    const { Search } = Input;
+    const onSearch = (value) => console.log(value);
+
+
     return (
-        <nav>
+        <nav className="navbar">
+            <img className="hamburger" src={hamburger} alt="hamburger" />
             <ul>
                 <li>
-                    <Link to="/football">football</Link>
+                    <Link className="category-links" to="/football">Football</Link>
                 </li>
                 <li>
-                    <Link to="/basketball">Basketball</Link>
+                    <Link className="category-links" to="/basketball">Basketball</Link>
                 </li>
                 <li>
-                    <Link to="/rugby">Rugby</Link></li>
+                    <Link className="category-links" to="/rugby">Rugby</Link></li>
                 <li>
-                    <Link to="/cycling">Cycling</Link>
+                    <Link className="category-links" to="/cycling">Cycling</Link>
                 </li>
                 <li>
-                    <Link to="/tennis">Tennis</Link>
+                    <Link className="category-links" to="/tennis">Tennis</Link>
                 </li>
             </ul>
-
+            <Search
+                placeholder="Search"
+                onSearch={onSearch}
+                style={{
+                    width: 200,
+                }}
+            />
 
         </nav>
     )
