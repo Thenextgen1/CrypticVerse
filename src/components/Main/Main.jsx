1a8ed00657980cced0779664ddc9c1ce9dc02542
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-05-25 10:27:32 +0100

    restructuring

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import { Col, Row } from 'antd';
 import { Link } from 'react-router-dom';
 import '../../styles/main.css'
-import football from '../../assets/football.jpg'
-import basketball from '../../assets/basketball.jpg'
-import cycling from '../../assets/cycling.jpg'
-import rugby from '../../assets/rugby.jpg'
-
 
 
 const Main = () => {
@@ -14,29 +9,24 @@ const Main = () => {
         <main className='main'>
             <Row className='sport-images-container'>
                 <Col span={12}>
-                    <img src={cycling} alt="cycling" />
-                    <div>
-                        {/* <Link className="" to="/cycling">Cycling</Link>
-                        <p>Lorem, ipsum dolor.</p> */}
+                    <div className='cycling-container'>
+                        <Link className="cycling-link" to="/cycling">Cycling</Link>
+                        <p>Lorem, ipsum dolor.</p>
                     </div>
                 </Col>
-                <Col span={12}>
+                <Col span={12} className="allsports-showcase">
                     <div className="football-container">
-                        <img src={football} alt="football" className='football' />
-                        {/* <Link to="/football">Football</Link>
-                        <p>Lorem, ipsum dolor.</p> */}
+                        <Link to="/football" className='football-link'>Football</Link>
+                        <p>Lorem, ipsum dolor.</p>
                     </div>
-
                     <div className='sport-showcase'>
                         <div className="basketball-container">
-                            <img src={basketball} alt="basketball" />
-                            {/* <Link to="/basketball">Basketball</Link>
-                            <p>Lorem, ipsum dolor.</p> */}
+                            <Link to="/basketball" className='basketball-link'>Basketball</Link>
+                            <p>Lorem, ipsum dolor.</p>
                         </div>
                         <div className="rugby-container">
-                            <img src={rugby} alt="rugby" />
-                            {/* <Link to="/rugby">Rugby</Link>
-                            <p>Lorem, ipsum dolor.</p> */}
+                            <Link to="/rugby" className='rugby-link'>Rugby</Link>
+                            <p>Lorem, ipsum dolor.</p>
                         </div>
 
 
